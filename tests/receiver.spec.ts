e665874eebe923df459bdcce53e49d8ef74f8d22
Shigma <1700011071@pku.edu.cn>
2019-12-01 20:07:04 +0800

    middleware specifications

--- tests/receiver.spec.ts
+++ tests/receiver.spec.ts
@@ -1,11 +1,17 @@
 import { SERVER_URL, CLIENT_PORT, createServer, postMeta } from './utils'
-import { createApp, App } from '../src'
+import { createApp, App, Meta } from '../src'
 import { Server } from 'http'
 
 let app: App
 let server: Server
 
 jest.setTimeout(1000)
+
+const shared: Meta = {
+  postType: 'message',
+  userId: 10000,
+  selfId: 514,
+}
 
 beforeAll(() => {
   server = createServer()
@@ -46,14 +52,12 @@ describe('receiver', () => {
 
   test('friend', async () => {
     await postMeta({
-      postType: 'message',
-      userId: 10000,
-      selfId: 514,
+      ...shared,
       messageType: 'private',
       subType: 'friend',
       message: 'Hello',
     })
-  
+
     mocks.slice(0, 2).forEach(func => expect(func).toBeCalledTimes(1))
     mocks.slice(2, 3).forEach(func => expect(func).toBeCalledTimes(0))
     mocks.slice(3, 7).forEach(func => expect(func).toBeCalledTimes(1))
@@ -62,16 +66,90 @@ describe('receiver', () => {
 
   test('group', async () => {
     await postMeta({
-      postType: 'message',
-      groupId: 10000,
-      selfId: 514,
+      ...shared,
       messageType: 'group',
       subType: 'normal',
       message: 'World',
     })
-  
+
     mocks.slice(0, 1).forEach(func => expect(func).toBeCalledTimes(2))
     mocks.slice(1, 3).forEach(func => expect(func).toBeCalledTimes(1))
     mocks.slice(3, 11).forEach(func => expect(func).toBeCalledTimes(1))
+  })
+})
+
+describe('middleware', () => {
+  let flag: number
+
+  beforeEach(() => flag = 0)
+
+  beforeAll(() => {
+    app.users.middleware((_, next) => {
+      flag |= 1 << 1
+      return next()
+    })
+
+    app.groups.middleware(({ message }, next) => {
+      flag |= 1 << 2
+      if (message === 'foo') return
+      if (message === 'bar') return next()
+      return next(() => (flag |= 1 << 4, undefined))
+    })
+
+    app.middleware(({ message }, next) => {
+      flag |= 1 << 3
+      if (message === 'foo') return next()
+      if (message === 'bar') return
+      return next(next => (flag |= 1 << 5, next(() => (flag |= 1 << 6, undefined))))
+    })
+
+    app.middleware((_, next) => {
+      flag |= 1 << 0
+      return next()
+    }, 0)
+  })
+
+  test('middleware', async () => {
+    await postMeta({
+      ...shared,
+      messageType: 'private',
+      subType: 'friend',
+      message: 'foo',
+    })
+
+    expect(flag.toString(2).split('').reverse().join('')).toBe('1101')
+  })
+
+  test('middleware', async () => {
+    await postMeta({
+      ...shared,
+      messageType: 'group',
+      subType: 'normal',
+      message: 'bar',
+    })
+
+    expect(flag.toString(2).split('').reverse().join('')).toBe('1011')
+  })
+
+  test('middleware', async () => {
+    await postMeta({
+      ...shared,
+      messageType: 'private',
+      subType: 'friend',
+      message: 'baz',
+    })
+
+    expect(flag.toString(2).split('').reverse().join('')).toBe('1101011')
+  })
+
+  test('middleware', async () => {
+    await postMeta({
+      ...shared,
+      messageType: 'group',
+      subType: 'normal',
+      message: 'baz',
+    })
+
+    expect(flag.toString(2).split('').reverse().join('')).toBe('10111')
   })
 })
